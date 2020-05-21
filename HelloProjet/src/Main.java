bad9742e35c777d2341ca2a3f6b18ead8175d155
Lucifer <CNLuciferYH@gmail.com>
2020-05-21 18:09:01 +0800

    修改了一些内容

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
+
         System.out.println("HelloWorld!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main");
     }
 }
